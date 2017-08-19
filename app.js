4c0fd1e97affbf1fbde5f7cc910e8ec9f8a0978e
Safarali <mr.safarali@gmail.com>
2017-08-19 00:11:24 -0700

    Add percentages on UI

--- app.js
+++ app.js
@@ -138,7 +138,8 @@ var UIController = (function() {
         incomeLabel: '.budget__income--value',
         expLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel: '.item__percentage'
 
     }
     return {
@@ -197,6 +198,22 @@ var UIController = (function() {
             }
         },
 
+        displayPercentages: function (percentages) {
+            var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+            var nodeListForEach = function(list, callback) {
+                for (var i = 0; i < list.length; i++) {
+                    callback(list[i], i);
+                }
+            };
+            nodeListForEach(fields, function(current, index) {
+                if (percentages[index] > 0) {
+                    current.textContent = percentages[index] + '%';
+                } else {
+                    current.textContent = '---';
+                }
+            });
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -234,7 +251,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         // Read from budgetCtrl
         var percentages = budgetCtrl.getPercentages();
         // Update UI with new percentage
-        console.log(percentages);
+        UICtrl.displayPercentages(percentages);
     };
 
     var ctrlAddItem = function() {
